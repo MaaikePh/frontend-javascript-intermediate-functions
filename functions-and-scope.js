a6d0669e6e9540af8f6a5e047bc5358c58fe8c58
MaaikePh <maaikephielix@live.nl>
2025-07-01 16:03:13 +0200

    functions-and-scope bonusopdracht gemaakt

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -124,12 +124,24 @@ console.log(averageGradeRounded([8, 9, 4, 6, 10]));
 /* 3a: Script schrijven  */
 // Schrijf een script die op basis van de grades array (hierboven) checkt wat het hoogst behaalde cijfer is. Je mag hier geen bestaande methoden voor gebruiken. Schrijf de stapjes eerst uit en vraag jezelf de volgende dingen af:
 // * Hoe kan ik iedere waarde van de array langsgaan?
+// Met een for-loop.
 // * Op welke conditie moet ik checken?
+// Welk cijfer uit de array is het hoogste. Dit kan door te kijken of het huidige cijfer in de loop hoger is dan het hoogst opgeslagen cijfer.
 // * Hoe zorg ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan opslaan?
+// Deze kan je opslaan in een let variabele. Deze kan dus overschreven worden elke keer dat de loop wordt doorlopen.
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 9
 
+let grade = grades[0];
+
+for (let i = 1; i < grades.length; i++) {
+    if (grades[i] > grade) {
+        grade = grades[i];
+    }
+}
+
+console.log(grade);
 
 /* 3b: Omschrijven tot een herbruikbare functie */
 // Schrijf een functie genaamd highestGrade, die een array van cijfers verwacht (zoals grades) en het hoogste cijfer teruggeeft. Gebruik hiervoor jouw antwoord van 3a.
@@ -140,3 +152,17 @@ console.log(averageGradeRounded([8, 9, 4, 6, 10]));
 // highestGrade(grades) geeft 9
 // highestGrade([6, 4, 5]) geeft 6
 // highestGrade([8, 9, 4, 6, 10]) geeft 10
+
+function highestGrade(grades) {
+    let grade = grades[0];
+    for (let i = 1; i < grades.length; i++) {
+        if (grades[i] > grade) {
+            grade = grades[i];
+        }
+    }
+    return grade;
+}
+
+console.log(highestGrade(grades));
+console.log(highestGrade([6, 4, 5]));
+console.log(highestGrade([8, 9, 4, 6, 10]));
